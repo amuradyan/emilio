a1718ce3826d680168202e9de71f830a2d979a2c
darthspectrum <muradianator@gmail.com>
2024-10-08 10:55:29 +0000

    first: an empty project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject emilio "0.1.0-SNAPSHOT"
+  :description "Emilio Hernandez-Azatyan is a Telegram bot over OpenAI's assistant"
+  :url "https://github.com/amuradyan/emilio"
+  :license {:name         "Zero-Clause BSD"
+            :url          "https://opensource.org/license/0bsd"
+            :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]]
+  :repl-options {:init-ns emilio.core}
+  :main emilio.core)
